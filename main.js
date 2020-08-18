fb9daba3b79f2eceb27dde674a8d47a05cc0832e
Ritaivko <rita26111@gmail.com>
2020-08-19 00:11:06 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict"
+alert('Я JavaScript!');
+let admin = 'Джон';
+let name = 'Джон';
+alert('admin');
+let planetEarth;
+let user;
+let s = 'моя' + 'строка';
+alert(s);
+alert('You name?');
+let n = prompt('You name?', '');
+alert(n);
+let na = confirm ('You name?');
+alert(na);
